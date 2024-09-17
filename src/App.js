f12250a8059f3c26f6879ebb34f28bf2539e8646
Anıl Başaran <nlbsrn@gmail.com>
2024-09-18 02:00:57 +0300

    added a 2 second delay to showcase the loading spinning image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 
 import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import TeamCards from "./TeamCards.jsx";
 import {useState,useEffect} from "react";
 
 const App = () => {
 
   const [teamInfo, setTeamInfo] = useState([]);
+  const [isLoading,setIsLoading] = useState(true);
   
-  const fetchInfo=()=>{
+/*   const fetchInfo=()=>{
 fetch("https://api.react-formula.com/learning-api/demos/teammates-project/profiles")
   .then((response)=>response.json())
-  .then((data)=> setTeamInfo(data) )
-}
+  .then((data)=> {
+    setTeamInfo(data);
+    setIsLoading(false);
+  }
+  )
+} */
+
+  const fetchInfo = () => {
+    setTimeout(() => {
+      fetch("https://api.react-formula.com/learning-api/demos/teammates-project/profiles")
+        .then((response) => response.json())
+        .then((data) => {
+          setTeamInfo(data);
+          setIsLoading(false);
+        });
+    }, 2000); // 2 seconds delay
+  };
+  
 
   useEffect(()=>{
   fetchInfo();
@@ -20,10 +38,14 @@ fetch("https://api.react-formula.com/learning-api/demos/teammates-project/profil
   console.log(teamInfo)
   const allTeam = teamInfo.map((member,index)=><TeamCards key={index} info={member} />)
   
-  return <div className="bg-neutral-100 flex min-h-screen justify-center">
-  <div className="innerContainer" >
-    {allTeam}
-  </div>
+  return <div className="bg-neutral-100 flex min-h-screen justify-center items-center">
+ {
+    isLoading ? (<FontAwesomeIcon icon={faSpinner} className='text-6xl text-teal-400 animate-spin' />) : ( <div className="innerContainer" >
+      {allTeam}
+    </div>)
+
+
+ }
   </div>
 };
 
